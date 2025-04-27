c4fb7445a1d0d2ee1810b3ce8f6cdedb4fda70ec
kishan gupta <kkgupta0106@gmail.com>
2025-04-28 00:48:20 +0530

    add webcam

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { db } from "@/utils/db";
 import { MockInterview } from "@/utils/schema";
 import { eq } from "drizzle-orm";
+import { WebcamIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
 
 const Interview = ({ params }) => {
   const [interviewData, setInterviewData] = useState(null);
+  const [webCamEnabled, setwebCamEnabled] = useState(false);
 
   useEffect(() => {
     // Define a new async function inside useEffect
@@ -32,8 +36,27 @@ const Interview = ({ params }) => {
   }, [params.interviewId]); // ✅ Dependency added
 
   return (
-    <div className="my-10 flex justify-center">
+    <div className="my-10 flex justify-center flex-col items-center">
       <h2 className="font-bold text-2xl">Lets's Get Started</h2>
+      <div>
+        {webCamEnabled ? (
+          <Webcam
+            onUserMedia={() => {
+              setwebCamEnabled(true);
+            }}
+            onUserMediaError={() => setwebCamEnabled(false)}
+            mirrored={true}
+            style={{ height: 300, width: 300 }}
+          />
+        ) : (
+          <>
+            <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border " />
+            <Button onClick={()=>setwebCamEnabled(true)}>
+              Enable Web Cam and Microphone
+            </Button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
